9b58c1f851320edb82df98c04674dc7ea5029d03
atharvjamkhindikar <jamkhindikaratharva5.com>
2026-01-31 12:29:20 +0530

    cleanup

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
+const API_BASE_URL = 'http://ec2-13-220-57-64.compute-1.amazonaws.com:8080/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
